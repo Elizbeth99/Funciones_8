6f429d8375c6be413cd948765e8124e61fe62dd6
Eli <113862594+Elizbeth99@users.noreply.github.com>
2022-11-29 02:20:40 -0600

    Add files via upload

---
+++ Funciones.js
@@ -0,0 +1,18 @@
+function DevuelvaTrue() 
+{
+  return true
+}
+
+async function damePromesa()
+{
+  return setTimeout(() => console.log("Hola soy una promesa"), 5000)
+}
+
+function* IndParesAuto() 
+{
+    let id = 0
+    while(true) 
+    {
+      yield id += 2
+    }
+}
